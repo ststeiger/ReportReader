fd911066e66ff8c97e0c629f794d422c90a2c138
ststeiger <quandary82@hailmail.net>
2018-08-20 17:00:39 +0200

    Xmlify

--- ReportReaderVB/Program.vb
+++ ReportReaderVB/Program.vb
@@ -1,10 +1,29 @@
-Imports System
 
 Module Program
 
 
+    Function Xmlify(args As Object()) As String
+        Dim str As String = Nothing
+
+        Dim sb As New System.Text.StringBuilder
+        For i As Integer = 0 To args.Length - 1 Step 1
+            Dim s As String = System.Convert.ToString(args(i))
+            If (String.IsNullOrEmpty(s)) Then
+                Continue For
+            End If
+
+            sb.Append("<e>")
+            sb.Append(s)
+            sb.Append("</e>")
+        Next i
+        str = sb.ToString()
+
+        Return str
+    End Function
+
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine(Xmlify(New String() {"abc", Nothing, "def", "ghi", "jkl", "mno"}))
 
         System.Console.WriteLine(System.Environment.NewLine)
         System.Console.WriteLine(" --- Press any key to continue --- ")
