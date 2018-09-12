149cedb02a4c67ab407ecd6a187d9e3c26cc82d3
ststeiger <quandary82@hailmail.net>
2018-09-12 16:50:46 +0200

    ToDateString for report

---
+++ ReportReaderVB/Code.vb
@@ -0,0 +1,42 @@
+﻿
+Public Class RsCode
+
+
+    Function ToDateString(dateStringObject As Object) As String
+        Dim dateString As String = System.Convert.ToString(dateStringObject, System.Globalization.CultureInfo.InvariantCulture)
+
+        If String.IsNullOrEmpty(dateString) Then
+            Return ""
+        End If
+
+        Dim x As System.DateTime
+        If System.DateTime.TryParseExact(dateString, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture, Globalization.DateTimeStyles.None, x) Then
+            Return x.ToString("dd-MMM-yyyy", System.Globalization.CultureInfo.InvariantCulture)
+        End If
+
+
+        Return dateString
+    End Function
+
+
+    Function Xmlify(ParamArray args As Object()) As String
+        Dim str As String = Nothing
+
+        Dim sb As New System.Text.StringBuilder
+        For i As Integer = 0 To args.Length - 1 Step 1
+            Dim s As String = System.Convert.ToString(args(i))
+            If (String.IsNullOrEmpty(s)) Then
+                Continue For
+            End If
+
+            sb.Append("<e>")
+            sb.Append(s)
+            sb.Append("</e>")
+        Next i
+        str = sb.ToString()
+
+        Return str
+    End Function
+
+
+End Class
--- ReportReaderVB/Program.vb
+++ ReportReaderVB/Program.vb
@@ -2,53 +2,20 @@
 Module Program
 
 
-    Function Xmlify(args As Object()) As String
-        Dim str As String = Nothing
-
-        Dim sb As New System.Text.StringBuilder
-        For i As Integer = 0 To args.Length - 1 Step 1
-            Dim s As String = System.Convert.ToString(args(i))
-            If (String.IsNullOrEmpty(s)) Then
-                Continue For
-            End If
-
-            sb.Append("<e>")
-            sb.Append(s)
-            sb.Append("</e>")
-        Next i
-        str = sb.ToString()
-
-        Return str
-    End Function
-
-
-    Friend ReadOnly InvalidHandle As System.IntPtr = System.IntPtr.Zero
-
-
-    Sub ReadRessourceFile()
-        ''#Requires Assembly System.Windows.Forms 
-        'Dim rsxr As System.Resources.ResXResourceReader = New System.Resources.ResXResourceReader("items.resx")
-
-        '''# Iterate through the resources and display the contents to the console.    
-        'Dim d As System.Collections.DictionaryEntry
-        'For Each d In rsxr
-        '    Console.WriteLine(d.Key.ToString() + ":" + ControlChars.Tab + d.Value.ToString())
-        'Next d
-
-        '''#Close the reader. 
-        'rsxr.Close()
-    End Sub
-
     Sub Main(args As String())
-        Console.WriteLine(Xmlify(New String() {"abc", Nothing, "def", "ghi", "jkl", "mno"}))
+        Dim Code As New RsCode()
+
+        ' Dim strXml As String = Code.Xmlify("a", "b", "c")
+        ' System.Console.WriteLine(Code.Xmlify(New String() {"abc", Nothing, "def", "ghi", "jkl", "mno"}))
+
+        'MOST_RECENT_HIRE_DATE
+        Dim str As String = Code.ToDateString("20121201")
+        System.Console.WriteLine(str)
 
 
 
-
-
-
-        Dim opts As New System.CodeDom.Compiler.CompilerParameters()
-        System.Console.WriteLine(opts.CoreAssemblyFileName)
+        ' Dim opts As New System.CodeDom.Compiler.CompilerParameters()
+        ' System.Console.WriteLine(opts.CoreAssemblyFileName)
 
 
         System.Console.WriteLine(System.Environment.NewLine)
